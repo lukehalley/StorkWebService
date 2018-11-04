227088f10973c16d0b7e6a4ed83aa220320023e0
lukehalley <luke123halley@gmail.com>
2018-11-04 09:10:23 +0800

    app setup - added basic html page

--- stork-web-app-service/src/app/app.module.ts
+++ stork-web-app-service/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StorkCreateComponent } from './storks/stork-create/stork-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StorkCreateComponent
   ],
   imports: [
     BrowserModule
