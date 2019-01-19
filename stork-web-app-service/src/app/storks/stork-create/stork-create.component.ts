b7cb83a972ed873a1f4dae8bb2d460700e99d26a
lukehalley <luke123halley@gmail.com>
2019-01-19 13:37:46 +0000

    added basic form validation

--- stork-web-app-service/src/app/storks/stork-create/stork-create.component.ts
+++ stork-web-app-service/src/app/storks/stork-create/stork-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { Renderer2 } from '@angular/core';
 import { Stork } from '../stork.model';
 
 @Component({
@@ -7,15 +8,46 @@ import { Stork } from '../stork.model';
   templateUrl: './stork-create.component.html'
 })
 export class StorkCreateComponent {
+  // Input field values
   enteredStork_id = '';
   enteredNickname = '';
+
+  // Init classes for input fields
+  public idInput = 'input';
+  public nicknameInput = 'input';
+
+  // Readymade classes to set inputs as error or ok
+  public normalInput = 'input';
+  public errorInput = 'input is-danger';
+
+  // Create the emmiter to update the values system wide
   @Output() storkCreated = new EventEmitter<Stork>();
 
-  onAddStork() {
-    const stork: Stork = {
-      stork_id: this.enteredStork_id,
-      nickname: this.enteredNickname
-    };
-    this.storkCreated.emit(stork);
+  // Create the button press listener
+  onAddStork(form: NgForm) {
+
+    const idLen = form.value.inputStorkID.length;
+    const nickLen = form.value.inputStorkNickname.length;
+
+    const idType = typeof form.value.inputStorkID;
+    const nickType = typeof form.value.inputStorkNickname;
+
+    if (form.valid) {
+      this.idInput = this.normalInput;
+      const stork: Stork = {
+        stork_id: form.value.inputStorkID,
+        nickname: form.value.inputStorkNickname
+      };
+      this.storkCreated.emit(stork);
+    } else {
+      if (idLen !== 7 || idType !== 'string') {
+        this.idInput = this.errorInput;
+      }
+      if (nickLen > 20 || nickLen === 0 || nickType !== 'string') {
+        this.nicknameInput = this.errorInput;
+      }
+      return;
+    }
+
   }
 }
