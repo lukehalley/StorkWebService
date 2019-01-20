f469b9c3a2440f87692e96adfbddee709c610874
lukehalley <luke123halley@gmail.com>
2019-01-20 16:31:01 +0000

    get request now working and pulling dummy storks from backend using http

--- stork-web-app-service/backend/app.js
+++ stork-web-app-service/backend/app.js
@@ -1,16 +1,29 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.use("/api/storks", (req, res, next) => {
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, PUT, OPTIONS'
+  );
+  next();
+});
+
+app.use('/api/storks', (req, res, next) => {
   const storks = [
-    { stork_id: "STR0001", nickname: "MonaLisa" },
-    { stork_id: "STR0002", nickname: "LukesStork" },
-    { stork_id: "STR0003", nickname: "GwynnsStork" }
+    { stork_id: 'STR0001', nickname: 'MonaLisa' },
+    { stork_id: 'STR0002', nickname: 'LukesStork' },
+    { stork_id: 'STR0003', nickname: 'GwynnsStork' }
   ];
   // 200 = Success
   res.status(200).json({
-    message: "Storks fetched sucessfully",
+    message: 'Storks fetched sucessfully',
     storks: storks
   });
 });
--- stork-web-app-service/src/app/storks/storks.service.ts
+++ stork-web-app-service/src/app/storks/storks.service.ts
@@ -1,17 +1,24 @@
 import { Stork } from './stork.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class StorksService {
   private storks: Stork[] = [];
   private storksUpdated = new Subject<Stork[]>();
 
+  constructor(private http: HttpClient) {}
+
   getStorks() {
-    // Using the spread operator to create a new array and take all the elements
-    // from storks and put them in the new array. This will keep the original array
-    // the same and safe.
-    return [...this.storks];
+    this.http
+      .get<{ message: string; storks: Stork[] }>(
+        'http://localhost:3000/api/storks'
+      )
+      .subscribe(storkData => {
+        this.storks = storkData.storks;
+        this.storksUpdated.next([...this.storks]);
+      });
   }
 
   getStorksUpdateListener() {
@@ -19,9 +26,8 @@ export class StorksService {
   }
 
   addStork(stork_id: string, nickname: string) {
-    const stork: Stork = {stork_id: stork_id, nickname: nickname};
+    const stork: Stork = { stork_id: stork_id, nickname: nickname };
     this.storks.push(stork);
     this.storksUpdated.next([...this.storks]);
   }
-
 }
