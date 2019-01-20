8b46b8a53d96246f54db5815531c50e1e703a3b3
lukehalley <luke123halley@gmail.com>
2019-01-20 15:57:23 +0000

    Basic route set up with dummy data and set nodemon as dev dep

--- stork-web-app-service/backend/app.js
+++ stork-web-app-service/backend/app.js
@@ -1,15 +1,18 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('One');
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Two');
-  res.send('Hello from Stork');
+app.use("/api/storks", (req, res, next) => {
+  const storks = [
+    { stork_id: "STR0001", nickname: "MonaLisa" },
+    { stork_id: "STR0002", nickname: "LukesStork" },
+    { stork_id: "STR0003", nickname: "GwynnsStork" }
+  ];
+  // 200 = Success
+  res.status(200).json({
+    message: "Storks fetched sucessfully",
+    storks: storks
+  });
 });
 
 module.exports = app;
