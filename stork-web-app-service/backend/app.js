670cb76a9e83f156d14e060226acc9a908a24227
lukehalley <luke123halley@gmail.com>
2019-01-20 16:52:48 +0000

    added body parser and adding POST

--- stork-web-app-service/backend/app.js
+++ stork-web-app-service/backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -13,6 +15,15 @@ app.use((req, res, next) => {
     'GET, POST, PATCH, DELETE, PUT, OPTIONS'
   );
   next();
+});
+
+app.post('/api/storks', (req, res, next) => {
+  const storks = req.body;
+  console.log(storks);
+  // 200 = Success & Something Was Created
+  res.sendStatus(201).json({
+    message: 'Stork added sucessfully!'
+  });
 });
 
 app.use('/api/storks', (req, res, next) => {
