51e3e0bfc9ba3d37258ecaf3a757daaddec7fc84
lukehalley <luke123halley@gmail.com>
2019-01-22 19:14:59 +0000

    added basic routing

---
+++ stork-web-app-service/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StorkListComponent } from './storks/stork-list/stork-list.component';
+import { StorkCreateComponent } from './storks/stork-create/stork-create.component';
+
+const routes: Routes = [
+  { path: 'your-storks', component: StorkListComponent },
+  { path: 'register-stork', component: StorkCreateComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
