6beb530fbabb8900c1791b390b3b987f80072cb8
lukehalley <luke123halley@gmail.com>
2019-01-26 16:40:06 +0000

    users now being signed up

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,79 +1,38 @@
 const express = require('express');
+const User = require('../models/user.js');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 // Create a User and send it to the database
 // to be stored
 router.post('/signup', (req, res, next) => {
-  const user = new User({
-    stork_code: req.body.stork_code,
-    nickname: req.body.nickname
-  });
-  console.log(stork);
-  stork.save().then(createdStork => {
-    // 201 = Success & Something Was Created
-    res.status(201).json({
-      message: 'Stork added sucessfully!',
-      storkId: createdStork._id
+  bcrypt.hash(req.body.password, 10).then(hashedPassword => {
+    const user = new User({
+      fname: req.body.fname,
+      sname: req.body.sname,
+      address: req.body.address,
+      phoneNumber: req.body.phoneNumber,
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+      plan: req.body.plan
     });
-  });
-});
-
-// Create a Stork device and send it to the database
-// to be stored
-router.put('/:id', (req, res, next) => {
-  const stork = new Stork({
-    _id: req.body.id,
-    stork_code: req.body.stork_code,
-    nickname: req.body.nickname
-  });
-  Stork.updateOne({ _id: req.params.id }, stork).then(result => {
-    console.log(result);
-    res.status(200).json({ message: 'Update successful!' });
-  });
-});
-
-// Get ALL Storks from the database and return them in the response
-router.get('', (req, res, next) => {
-  Stork.find()
-    .then(documents => {
-      console.log('Found: ' + documents);
-      // 200 = Success
-      res.status(200).json({
-        message: 'Storks fetched sucessfully',
-        storks: documents
+    console.log(user);
+    user
+      .save()
+      .then(createdUser => {
+        // 201 = Success & Something Was Created
+        res.status(201).json({
+          message: 'User added sucessfully!',
+          userId: createdUser._id
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
       });
-    })
-    .catch(e => {
-      console.error('Failed To Get ALL Documents From Database!');
-      console.error(e);
-    });
-});
-
-router.get('/:id', (req, res, next) => {
-  Stork.findById({ _id: req.params.id })
-    .then(stork => {
-      if (stork) {
-        res.status(200).json(stork);
-      } else {
-        res.status(404).json({ message: 'Stork Not Found!' });
-      }
-    })
-    .catch(e => {
-      console.error('Failed To Delete A Document From Database!: ');
-      console.error(e);
-    });
-});
-
-router.delete('/:id', (req, res, next) => {
-  Stork.deleteOne({ _id: req.params.id })
-    .then(result => {
-      console.log('Result: ' + result);
-      res.status(200).json({ message: 'Stork Deleted!' });
-    })
-    .catch(e => {
-      console.error('Failed To Delete A Document From Database!: ');
-      console.error(e);
-    });
+  });
 });
 
 module.exports = router;
