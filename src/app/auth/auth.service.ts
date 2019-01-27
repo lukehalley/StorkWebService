c2f6fe63eb73a4ebd9be17b0bc7a25bdbb6ffdc9
lukehalley <luke123halley@gmail.com>
2019-01-27 18:34:28 +0000

    changed header back to be a constant part of the app.component.html to reflect the login status better

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { Subject } from 'rxjs';
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 
@@ -8,9 +9,14 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
   private token: string;
   constructor(private http: HttpClient) {}
+  private authStatusListener = new Subject<boolean>();
 
   getToken() {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   createUser(
@@ -40,21 +46,6 @@ export class AuthService {
       });
   }
 
-  // login(email: string, password: string) {
-  //   const authData: AuthData = {
-  //     email: email,
-  //     password: password
-  //   };
-  //   this.http
-  //     .post<{ token: string }>(
-  //       'http://localhost:3000/api/users/login',
-  //       authData
-  //     )
-  //     .subscribe(response => {
-  //       console.log(response);
-  //       const token = response.token;
-  //     });
-  // }
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     this.http
@@ -63,8 +54,11 @@ export class AuthService {
         authData
       )
       .subscribe(response => {
+        // Getting the token from the response data
         const token = response.token;
         this.token = token;
+        // Informing the Stork app that the user is logged in
+        this.authStatusListener.next(true);
       });
   }
 }
--- src/app/toolbar/header.component.ts
+++ src/app/toolbar/header.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { AuthService } from './../auth/auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class StorkHeaderComponent {
+export class StorkHeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        console.log('User isAuthenticated: ' + isAuthenticated);
+        this.userIsAuthenticated = isAuthenticated;
+      });
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
+  }
 }
