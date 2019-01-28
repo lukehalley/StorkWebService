776ba35d7293e44f528bd2ca2e139f858e0eda7a
lukehalley <luke123halley@gmail.com>
2019-01-28 16:26:32 +0000

    added route gaurds to every routes except login and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './auth/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StorkListComponent } from './storks/stork-list/stork-list.component';
@@ -7,15 +8,28 @@ import { StorkSignUpComponent } from './auth/signup/stork-sign-up.component';
 
 const routes: Routes = [
   { path: '', component: StorkLoginComponent },
-  { path: 'your-storks', component: StorkListComponent },
-  { path: 'register-stork', component: StorkCreateComponent },
-  { path: 'edit/:storkId', component: StorkCreateComponent },
+  {
+    path: 'your-storks',
+    component: StorkListComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'register-stork',
+    component: StorkCreateComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'edit/:storkId',
+    component: StorkCreateComponent,
+    canActivate: [AuthGuard]
+  },
   { path: 'login', component: StorkLoginComponent },
   { path: 'sign-up', component: StorkSignUpComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule {}
