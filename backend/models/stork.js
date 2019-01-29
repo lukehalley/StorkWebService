27fa633e9ae5ed980d04860ef62ce7e7514a8831
lukehalley <luke123halley@gmail.com>
2019-01-29 15:25:43 +0000

    added owner_id to Stork model

--- backend/models/stork.js
+++ backend/models/stork.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const storkSchema = mongoose.Schema({
   stork_code: { type: String, required: true },
-  nickname: { type: String, required: true }
+  owner_id: { type: String, required: true },
+  // Linking each Stork to a User using Mongoose:
+  nickname: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Stork', storkSchema);
