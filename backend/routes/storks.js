363447f1b7ca72ec5d0b032b4c06ef8ef1a75e2d
lukehalley <luke123halley@gmail.com>
2019-02-02 20:18:44 +0000

    added controllers for storks - done

--- backend/routes/storks.js
+++ backend/routes/storks.js
@@ -1,127 +1,26 @@
 const express = require('express');
-const Stork = require('../models/stork.js');
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 
 // Calling checkAuth to check token to see if current user should
 // be able to access all the below routes.
 
-// Create a Stork device and send it to the database to be stored.
-router.post('', checkAuth, (req, res, next) => {
-  const stork = new Stork({
-    stork_code: req.body.stork_code,
-    nickname: req.body.nickname,
-    ownerId: req.userData.userId
-  });
-  console.log(stork);
-  stork
-    .save()
-    .then(createdStork => {
-      // 201 = Success & Something Was Created
-      res.status(201).json({
-        message: 'Stork added sucessfully!',
-        storkId: createdStork._id
-      });
-    })
-    .catch(e => {
-      res.status(500).json({
-        message: 'Stork Registration Failed!',
-        comment: 'Please check you have correctly filled all fields.',
-        error: err
-      });
-    });
-});
+// Getting the routes from the controllers
+const StorkController = require('../controllers/storks');
 
-// Update a Stork device and update it in the database.
-router.put('/:id', checkAuth, (req, res, next) => {
-  const stork = new Stork({
-    _id: req.body.id,
-    stork_code: req.body.stork_code,
-    nickname: req.body.nickname
-  });
-  Stork.updateOne(
-    { _id: req.params.id, ownerId: req.userData.userId },
-    stork
-  ).then(result => {
-    // If nModified is greater than one on the result that means a field was edited.
-    // Using nModified to check if a user owns the Stork.
-    if (result.nModified > 0) {
-      res.status(200).json({ message: 'Update successful!' });
-    } else {
-      res
-        .status(401)
-        .json({
-          message: 'User Not Authorised To Edit This Stork!',
-          comment: 'Please Sign In to edit your Storks'
-        })
-        .catch(e => {
-          res.status(500).json({
-            message: 'Stork Update Failed!',
-            comment: 'Please check you have correctly filled all fields.',
-            error: err
-          });
-        });
-    }
-  });
-});
+// Create a Stork device and send it to the database to be stored.
+router.post('', checkAuth, StorkController.createStork);
 
 // Get ALL Storks from the database and return them in the response
-router.get('/:ownerId', checkAuth, (req, res, next) => {
-  Stork.find({ ownerId: req.params.ownerId })
-    .then(documents => {
-      // 200 = Success
-      res.status(200).json({
-        message: 'Storks Fetched Sucessfully',
-        storks: documents
-      });
-    })
-    .catch(e => {
-      res.status(500).json({
-        message: 'Stork Retrival Failed!',
-        comment: 'Failed To Get Your Storks From The Database!',
-        error: err
-      });
-    });
-});
+router.get('/:ownerId', checkAuth, StorkController.getUserStorks);
 
 // Get ONE Stork from the database and return them in the response
-router.get('/one/:id', checkAuth, (req, res, next) => {
-  Stork.findById({ _id: req.params.id })
-    .then(stork => {
-      console.log('GETTING STORK WITH ID OF: ' + req.params.id);
-      if (stork) {
-        res.status(200).json(stork);
-      } else {
-        res.status(404).json({ message: 'Stork Not Found!' });
-      }
-    })
-    .catch(e => {
-      res.status(500).json({
-        message: 'Stork Retrival Failed!',
-        comment: 'Failed To Get A Document From Database!',
-        error: err
-      });
-    });
-});
+router.get('/one/:id', checkAuth, StorkController.getOneStork);
 
-router.delete('/:id', checkAuth, (req, res, next) => {
-  Stork.deleteOne({ _id: req.params.id, ownerId: req.userData.userId })
-    .then(result => {
-      if (result.n > 0) {
-        res.status(200).json({ message: 'Stork Deleted!' });
-      } else {
-        res
-          .status(401)
-          .json({ message: 'User Not Authorised To Delete This Stork!' });
-      }
-    })
-    .catch(e => {
-      res.status(500).json({
-        message: 'Stork De-registration Failed!',
-        comment: 'Failed To De-registrater Your Storks!',
-        error: err
-      });
-    });
-});
+// Update a Stork device and update it in the database.
+router.put('/:id', checkAuth, StorkController.updateStork);
+
+// Delete ONE Stork from the database and return them in the response
+router.delete('/:id', checkAuth, StorkController.deleteAStork);
 
 module.exports = router;
