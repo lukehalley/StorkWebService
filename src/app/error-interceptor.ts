f3956ded3f3ad2e487691a61d215706b80f6355c
lukehalley <luke123halley@gmail.com>
2019-02-02 16:49:51 +0000

    added sweetalert2 and set it to pop up in the error interceptor

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -6,13 +6,24 @@ import {
 } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+// ES6 Modules or TypeScript
+import Swal from 'sweetalert2';
 
 export class ErrorInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.error(error);
-        alert(error.error.error.message);
+        let errorMessage = 'An Unknown Error Occured!';
+        if (error.error.message) {
+          // If the error response has a message set it.
+          errorMessage = error.error.message;
+        }
+        Swal.fire({
+          title: errorMessage,
+          text: errorMessage,
+          type: 'error',
+          confirmButtonText: 'Ok'
+        });
         return throwError(error);
       })
     );
