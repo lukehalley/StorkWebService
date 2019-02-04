383380ec64254d457205b4ee63932f549578b323
Luke Halley <luke123halley@gmail.com>
2019-02-04 15:01:10 +0000

    added simple login test and its working

--- test/test-login.ts
+++ test/test-login.ts
@@ -1,10 +1,14 @@
 import { Selector } from 'testcafe';
+import { ClientFunction } from 'testcafe';
+const getLocation = ClientFunction(() => document.location.href);
 
-fixture `Getting Started`
-    .page `http://devexpress.github.io/testcafe/example`;
+fixture `Login to Stork as a User.`
+    .page `http://87.44.18.111:4200`;
 
-test('My first test', async t => {
+test('Login to Stork as a User.', async t => {
     await t
-        .typeText('#developer-name', 'John Smith')
-        .click('#submit-button');
+        .typeText('input[name="inputUserLoginEmail"]', 'l')
+        .typeText('input[name="inputUserLoginPassword"]', 'p')
+        .click('form > button[type="submit"]')
+        .expect(getLocation()).eql('http://87.44.18.111:4200/storks/your-storks');
 });
