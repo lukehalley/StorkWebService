10c78f1fe6766ce46cdb07cb9fa07bc31b085f50
Luke Halley <luke123halley@gmail.com>
2019-02-09 16:16:56 +0000

    now storing selectors in variables

--- test/test-ui.ts
+++ test/test-ui.ts
@@ -23,22 +23,46 @@ const passInvalid = faker.internet.password();
 const addressInvalid = faker.address.streetAddress();
 const phoneInvalid = faker.phone.phoneNumber();
 
+const registerNavButton = 'div#storkNavbar a:nth-child(2)';
+const addStorkButton = 'p > button[type="submit"]';
+const editStorkButton = 'footer > a:nth-child(1)';
+const storkId = 'STR1234';
+const storkIdEdit = 'STR4321';
+const storkNickname = 'YourNickname1234';
+const storkNicknameEdit = 'YourNickname4321';
+const storkTitle =
+  'body > app-root > app-stork-create > div > div > div:nth-child(1) > div > p';
+
+// Inputs
+const emailField = 'input[name="inputUserLoginEmail"]';
+const usernameField = 'input[name="inputUserSignUpUsername"]';
+const passwordField = 'input[name="inputUserLoginPassword"]';
+const firstNameField = 'input[name="inputUserSignUpFName"]';
+const secondNameField = 'input[name="inputUserSignUpSName"]';
+const addressField = 'input[name="inputUserSignUpAddress"]';
+const phoneNumberField = 'input[name="inputUserSignUpPhoneNumber"]';
+const signupSubmitButton = 'p > button[type="submit"]';
+const loginSubmitButton = 'form > button[type="submit"]';
+// Stork
+const inputStorkID = 'input[name="inputStorkID"]';
+const inputStorkNickname = 'input[name="inputStorkNickname"]';
+
 test('Valid Sign Up + Sign In With Valid Credentials', async t => {
   await t
     .click('body > app-root > ng-component > section > div > div > p > a')
-    .typeText('input[name="inputUserSignUpUsername"]', usernameValid)
-    .typeText('input[name="inputUserLoginEmail"]', emailValid)
-    .typeText('input[name="inputUserLoginPassword"]', passValid)
-    .typeText('input[name="inputUserSignUpFName"]', fNameValid)
-    .typeText('input[name="inputUserSignUpSName"]', sNameValid)
-    .typeText('input[name="inputUserSignUpAddress"]', addressValid)
-    .typeText('input[name="inputUserSignUpPhoneNumber"]', phoneValid)
-    .click('p > button[type="submit"]')
+    .typeText(usernameField, usernameValid)
+    .typeText(emailField, emailValid)
+    .typeText(passwordField, passValid)
+    .typeText(firstNameField, fNameValid)
+    .typeText(secondNameField, sNameValid)
+    .typeText(addressField, addressValid)
+    .typeText(phoneNumberField, phoneValid)
+    .click(signupSubmitButton)
     .expect(getLocation())
     .eql(url + '/login')
-    .typeText('input[name="inputUserLoginEmail"]', emailValid)
-    .typeText('input[name="inputUserLoginPassword"]', passValid)
-    .click('form > button[type="submit"]')
+    .typeText(emailField, emailValid)
+    .typeText(passwordField, passValid)
+    .click(loginSubmitButton)
     .expect(getLocation())
     .eql(url + '/storks/your-storks');
 });
@@ -49,14 +73,14 @@ test('Sign Up With A Duplicate Email', async t => {
   ).exists;
   await t
     .click('body > app-root > ng-component > section > div > div > p > a')
-    .typeText('input[name="inputUserSignUpUsername"]', usernameInvalid)
-    .typeText('input[name="inputUserLoginEmail"]', emailValid)
-    .typeText('input[name="inputUserLoginPassword"]', passInvalid)
-    .typeText('input[name="inputUserSignUpFName"]', fNameInvalid)
-    .typeText('input[name="inputUserSignUpSName"]', sNameInvalid)
-    .typeText('input[name="inputUserSignUpAddress"]', addressInvalid)
-    .typeText('input[name="inputUserSignUpPhoneNumber"]', phoneInvalid)
-    .click('p > button[type="submit"]')
+    .typeText(usernameField, usernameInvalid)
+    .typeText(emailField, emailValid)
+    .typeText(passwordField, passInvalid)
+    .typeText(firstNameField, fNameInvalid)
+    .typeText(secondNameField, sNameInvalid)
+    .typeText(addressField, addressInvalid)
+    .typeText(phoneNumberField, phoneInvalid)
+    .click(signupSubmitButton)
     .expect(signupErrorExists)
     .ok()
     .click(
@@ -70,14 +94,14 @@ test('Sign Up With A Duplicate Username', async t => {
   ).exists;
   await t
     .click('body > app-root > ng-component > section > div > div > p > a')
-    .typeText('input[name="inputUserSignUpUsername"]', usernameValid)
-    .typeText('input[name="inputUserLoginEmail"]', emailInvalid)
-    .typeText('input[name="inputUserLoginPassword"]', passInvalid)
-    .typeText('input[name="inputUserSignUpFName"]', fNameInvalid)
-    .typeText('input[name="inputUserSignUpSName"]', sNameInvalid)
-    .typeText('input[name="inputUserSignUpAddress"]', addressInvalid)
-    .typeText('input[name="inputUserSignUpPhoneNumber"]', phoneInvalid)
-    .click('p > button[type="submit"]')
+    .typeText(usernameField, usernameValid)
+    .typeText(emailField, emailInvalid)
+    .typeText(passwordField, passInvalid)
+    .typeText(firstNameField, fNameInvalid)
+    .typeText(secondNameField, sNameInvalid)
+    .typeText(addressField, addressInvalid)
+    .typeText(phoneNumberField, phoneInvalid)
+    .click(signupSubmitButton)
     .expect(signupErrorExists)
     .ok()
     .click(
@@ -90,29 +114,18 @@ test('Sign In With Invalid Credentials', async t => {
     'Login Unsuccessful!'
   ).exists;
   await t
-    .typeText('input[name="inputUserLoginEmail"]', emailValid)
-    .typeText('input[name="inputUserLoginPassword"]', 'wrongpassword')
-    .click('form > button[type="submit"]')
+    .typeText(emailField, emailValid)
+    .typeText(passwordField, 'wrongpassword')
+    .click(loginSubmitButton)
     .expect(loginErrorExists)
     .ok();
 });
 
 test('Register, Edit and Delete A Stork', async t => {
-  const registerNavButton = 'div#storkNavbar a:nth-child(2)';
-  const addStorkButton = 'p > button[type="submit"]';
-  const editStorkButton = 'footer > a:nth-child(1)';
-  const storkId = 'STR1234';
-  const storkIdEdit = 'STR4321';
-  const storkNickname = 'YourNickname1234';
-  const storkNicknameEdit = 'YourNickname4321';
-  const storkTitle =
-    'body > app-root > app-stork-create > div > div > div:nth-child(1) > div > p';
-  const inputStorkID = 'input[name="inputStorkID"]';
-  const inputStorkNickname = 'input[name="inputStorkNickname"]';
   await t
-    .typeText('input[name="inputUserLoginEmail"]', emailValid)
-    .typeText('input[name="inputUserLoginPassword"]', passValid)
-    .click('form > button[type="submit"]')
+    .typeText(emailField, emailValid)
+    .typeText(passwordField, passValid)
+    .click(loginSubmitButton)
     .expect(getLocation())
     .eql(url + '/storks/your-storks')
     .click(registerNavButton)
