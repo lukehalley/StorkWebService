f6b6f581fe98667805948890ba6320a830fb22ca
lukehalley <luke123halley@gmail.com>
2019-04-09 19:29:18 +0800

    stork model done

--- stork-web-app-service/src/app/storks/stork-create/stork-create.component.ts
+++ stork-web-app-service/src/app/storks/stork-create/stork-create.component.ts
@@ -1,19 +1,20 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
+import { Stork } from '../stork.model';
+
 @Component({
   selector: 'app-stork-create',
   templateUrl: './stork-create.component.html'
 })
 export class StorkCreateComponent {
-
+  enteredStork_id = '';
   enteredNickname = '';
-  enteredStorkID = '';
-  @Output() storkCreated = new EventEmitter();
+  @Output() storkCreated = new EventEmitter<Stork>();
 
   onAddStork() {
-    const stork = {
-      nickname: this.enteredNickname,
-      storkid: this.enteredStorkID
+    const stork: Stork = {
+      stork_id: this.enteredStork_id,
+      nickname: this.enteredNickname
     };
     this.storkCreated.emit(stork);
   }
